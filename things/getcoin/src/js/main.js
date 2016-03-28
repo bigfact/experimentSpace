3b5bb5a5da3fc7bb97501b632904c1859336bc27
bigfact <bigfact0@gmail.com>
2016-03-28 15:42:48 +0800

    getcoin + 重新组织代码结构，备份

--- things/getcoin/src/js/main.js
+++ things/getcoin/src/js/main.js
@@ -1,36 +1,47 @@
-$(doc).ready(function() {
-    // 是否登录吧标记
-    var isLogin = false;
-    // 游戏开始检测按钮
-    $('.activetime button').on('touchstart click', function(e) {
-        
-        // 
-        // todo: 检测登录状态
-        // 
-        
-        isLogin && $('#makesure').show();
-        !isLogin && $('#login').show();
-    });
-    // 关闭 makesure 弹框
-    $('#makesure .close i').on('touchstart click', function() {
-        $('#makesure').hide();
-    });
+// 基础
+var touchAndClick = 'touchstart click';
+$(document).ready(function() {
+    // 
+    // 游戏对象
+    // 
+    var gameobj = new Game();
+    // 
     // 游戏开始按钮
-    $('#makesure .footer').on('touchstart click', function() {
+    // 
+    $('.activityTime button').on(touchAndClick, function(e) {
 
-        // 
-        // todo: 显示游戏模块，隐藏活动时间模块，活动介绍模块向下移动
-        //
+        // todo: 检测登录状态
 
-        $('.activetime').hide();
-        $('.gameblock').show();
-        $('.infoblock').addClass('infoblock2');
-        $('#makesure').hide();
-        $('.countdown').show();
+        Page.isLogin && $('#gameon').show();
+        !Page.isLogin && $('#login').show();
+    });
+    // 
+    // 登录跳转
+    // 
+    $('#login button').on(touchAndClick, function() {
+        $('#login').hide();
+        Page.isLogin = true;
+    });
+    // 
+    // 不开始游戏
+    // 
+    $('#gameon .close i').on(touchAndClick, function() {
+        $('#gameon').hide();
+    });
+    // 
+    // 游戏倒计时开始，倒计时结束后，游戏开始
+    // 
+    $('#gameon .footer').on(touchAndClick, function() {
+        // 游戏倒计时
         clock(4, 1000, function(va) {
             if (va == 0) {
                 $('.countdown').hide();
-                init();
+                // 游戏初始化
+                gameobj.init(function() {
+
+                    // todo: 游戏结束时执行
+
+                });
             }
             else {
                 var tmp = va - 1;
@@ -38,80 +49,154 @@ $(doc).ready(function() {
                 $('.countdown span').text(tmp);
             }
         });
+
+        $('.activityTime').hide();
+        $('.gameblock').show();
+        $('.infoblock').addClass('infoblock2');
+        $('#gameon').hide();
+        $('.countdown').show();
     });
-    // 登录跳转
-    $('#login button').on('touchstart click', function() {
-        $('#login').hide();
-        isLogin = true;
-    });
+
+
+
+
+
 });
 
-// 基础函数
-var doc = document;
-// 总时间，单位 s
-var timeall = 10;
-// 时间显示区域
-var tabtime = doc.getElementsByClassName('tabtime')[0];
-// 分数显示区域
-var tabpoint = doc.getElementsByClassName('tabpoint')[0];
-// 当前分数
-var point = 0;
-// 当前时间，单位 s
-var time = timeall;
-// 点容器
-var coins = null;
-// 两次点击之间的合理间隔值，单位 ms，用于限制点击过快，以已知最快点击速度（百度，高桥名人） 16/s 计算，每两次点击之间的间隔应限制为 60 毫秒左右
-var mintime = 60;
-// 当前点击时间，单位 ms，用于限制点击过快
-var timenow = 0;
-// 重置按钮
-var reset = doc.getElementsByClassName('reset')[0];
-// 初始化
-// init();
-// 初始化方法
-function init() {
-    timenow = 0;
-    point = 0;
-    time = timeall;
-    tabtime.innerText = timeall;
-    tabpoint.innerText = point;
-    reset.setAttribute('disabled', 'disabled');
-    coins != null && coins.parentNode.removeChild(coins);
-    coins = doc.createElement('div');
-    doc.getElementsByClassName('gameblock')[0].appendChild(coins);
-    reset.addEventListener('click', function(e) {
-        e.preventDefault();
-        e.stopPropagation();
-        time == 0 && init();
-    }, false);
-    reset.addEventListener('touchstart', function(e) {
-        e.preventDefault();
-        e.stopPropagation();
-        time == 0 && init();
-    }, false);
-    // 点击监听
-    doc.getElementsByClassName('gameblock')[0].addEventListener('click', onclick, false);
-    doc.getElementsByClassName('gameblock')[0].addEventListener('touchstart', onclick, false);
-    // 计时开始
-    point == 0 && clock(timeall, 1000, function(va) {
-        tabtime.innerText = va;
-        time = va;
-        time == 0 && reset.removeAttribute('disabled');
-    });
+
+// 
+// 页面对象
+// 
+function Page() {
+    // 是否登录标记
+    this.isLogin = false;
+    // // 弹层
+    // this.popup = {
+    //     // 登录提示
+    //     login: '',
+    //     // 游戏开始
+    //     gameon: '',
+    //     // 倒计时
+    //     countdown: '',
+    //     // 游戏结束
+    //     gameover: '',
+    //     // 短信发送
+    //     msgSend: '',
+    //     // 手机号错误
+    //     phoneError: '',
+    //     // 验证码错误
+    //     msgError: '',
+    //     // 金币领取成功
+    //     coinGeted: '',
+    //     // 金币达到 500
+    //     coinFull: '',
+    //     // 活动结束
+    //     activityEnd: ''
+    // }
 }
-// 点击执行方法
-function onclick(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    // 已经添加点击间隔时间限制，最小间隔时间为 mintime
-    if (time > 0 && Date.now() - timenow > mintime) {
-        // if(time > 0) {
-        timenow = Date.now();
-        point++;
-        newcoin(e);
+
+Page.prototype = {
+    // 
+    // 登录状态检查
+    // 
+    checkLogin: function() { },
+    // 
+    // 发送短信验证码
+    // 
+    sendMsg: function() { },
+    // 
+    // 提交收集的金币信息
+    // 
+    postGameData: function() { },
+    // 
+    // 活动时间检查
+    // 
+    checkactivityTime: function() { },
+    // 
+    // 获取当前用户参加该活动的信息
+    // 
+    getUserData: function() { }
+}
+
+// 
+// 游戏对象
+// 
+function Game() {
+    var ths = this;
+    // 游戏区域
+    ths.gameblock = document.getElementsByClassName('gameblock')[0];
+    // 总时间，单位 s
+    ths.timeall = 10;
+    // 时间显示区域
+    ths.tabtime = document.getElementsByClassName('tabtime')[0];
+    // 分数显示区域
+    ths.tabpoint = document.getElementsByClassName('tabpoint')[0];
+    // 当前分数
+    ths.point = 0;
+    // 当前时间，单位 s
+    ths.time = ths.timeall;
+    // 金币容器
+    ths.coinSet = null;
+    // 两次点击之间的合理间隔值，单位 ms，用于限制点击过快，以已知最快点击速度（百度，高桥名人） 16/s 计算，每两次点击之间的间隔应限制为 60 毫秒左右
+    ths.mintime = 60;
+    // 当前点击时间，单位 ms，用于限制点击过快
+    ths.timenow = 0;
+}
+Game.prototype = {
+    // 游戏初始化，传入游戏结束时执行的函数
+    init: function(gameover) {
+        var ths = this;
+        ths.timenow = 0;
+        ths.point = 0;
+        ths.time = ths.timeall;
+        ths.tabtime.innerText = ths.timeall;
+        ths.tabpoint.innerText = ths.point;
+        ths.coinSet != null && ths.coinSet.parentNode.removeChild(coins);
+        ths.coinSet = document.createElement('div');
+        ths.gameblock.appendChild(ths.coinSet);
+        // 点击监听
+        ths.gameblock.addEventListener('click', function(e) {
+            ths.onclick(e, ths);
+        }, false);
+        ths.gameblock.addEventListener('touchstart', function(e) {
+            ths.onclick(e, ths);
+        }, false);
+        // 计时开始
+        ths.point == 0 && clock(ths.timeall, 1000, function(va) {
+            ths.tabtime.innerText = va;
+            ths.time = va;
+            // 计时结束，游戏结束
+            if (ths.time == 0) {
+                (typeof gameover === 'function') && gameover();
+            }
+        });
+    },
+    // 点击执行方法
+    onclick: function(e, ths) {
+        e.preventDefault();
+        e.stopPropagation();
+        // 已经添加点击间隔时间限制，最小间隔时间为 mintime
+        if (ths.time > 0 && Date.now() - ths.timenow > ths.mintime) {
+            // if(time > 0) {
+            ths.timenow = Date.now();
+            ths.point++;
+            ths.newcoin(e);
+        }
+        ths.tabpoint.innerText = ths.point;
+    },
+    // 生成新的点
+    newcoin: function(e) {
+        var coin = document.createElement('div');
+        coin.className = 'coin';
+        coin.style.top = (getSpotPosition(e).y - 50) + 'px';
+        coin.style.left = (getSpotPosition(e).x - 50) + 'px';
+        this.coinSet.appendChild(coin);
     }
-    tabpoint.innerText = point;
 }
+
+// 
+// 公共方法
+// 
 // 计时方法 
 function clock(i, j, onchang) {
     (i > 0 && j > 0) && setTimeout(function() {
@@ -120,14 +205,6 @@ function clock(i, j, onchang) {
         onchang(i);
         clock(i, j, onchang);
     }, j);
-}
-// 生成新的点
-function newcoin(e) {
-    var coin = doc.createElement('div');
-    coin.className = 'coin';
-    coin.style.top = (getSpotPosition(e).y - 50) + 'px';
-    coin.style.left = (getSpotPosition(e).x - 50) + 'px';
-    coins.appendChild(coin);
 }
 // 根据事件获取当前触点的位置
 function getSpotPosition(e) {
@@ -140,3 +217,34 @@ function getSpotPosition(e) {
     touches.y = touches.y || e.clientY || e.pageY;
     return touches;
 }
+
+// // 
+// // 弹框
+// // 
+// function Popup() {
+//     this.config = {
+//         title: '',
+//         content: '',
+//         centerBtn: '',
+//         bottomBtn: ''
+//     }
+// }
+
+// Popup.prototype = {
+//     init: function() {
+//         var ths = this;
+//         var tmp = document.createElement('div');
+//         tmp.classList = 'popup';
+//         tmp.innerHTML = '<div class="main">'
+//             + '<p class="close"><i></i></p>'
+//             + '<p class="title">' + ths.config.title + '</p>'
+//             + '<div>' + ths.config.content + '</div>';
+//         centerBtn != '' && (tmp.innerHTML += '<button class="centerBtn">' + ths.config.centerBtn + '</button>');
+//         bottomBtn != '' && (tmp.innerHTML += '<button class="footerBtn">' + ths.config.bottomBtn + '</button>');
+//         tmp.innerHTML += '</div>';
+//         tmp.getElementsByClassName('close').addEventListener('click', function() { }, false);
+//         tmp.getElementsByClassName('centerBtn').addEventListener('click', function() { }, false);
+//         tmp.getElementsByClassName('footer').addEventListener('click', function() { }, false);
+//         document.body.appendChild(tmp);
+//     }
+// }
