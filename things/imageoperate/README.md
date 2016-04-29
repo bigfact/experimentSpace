d61128bb093c3c543dd13032407a78cf231de817
bigfact <bigfact0@gmail.com>
2016-04-29 13:16:06 +0800

    imageoperate + 1.1.0 + 分化出两个版本，新增移动端的操作

--- things/imageoperate/README.md
+++ things/imageoperate/README.md
@@ -1,17 +1,73 @@
-# TinyThings - imageoperate
+#Imageoperate
 图片操作的组件
 
 ## 功能
-    * 鼠标滚轮放大、缩小
-    * 鼠标点击拖动图片
-    * 图片翻转
-    
-## 参数
-* config                // 默认配置
-    * zoomTimes: 1,     // 缩放倍数
-    * maxWidth: 1980,   // 最大宽度
-    * minWidth: 320,    // 最小宽度
-    * degree: 90,       // 每次旋转的度数
-    * direction: 0      // 旋转方向及次数，正数为顺时针，负数为逆时针
+  * 鼠标滚轮或手势放大、缩小
+  * 鼠标点击或手势拖动图片
+  * 图片旋转
+  
+## imageoprate.js 参数及说明
+* 仅支持 PC 端
 
-## [例子](index.html)
+```js
+config = {            // 默认配置
+  zoomTimes: 1,       // 缩放倍数
+  maxWidth: 1980,     // 最大宽度
+  minWidth: 320,      // 最小宽度
+  degree: 90,         // 每次旋转的度数
+  direction: 0        // 旋转方向及次数，正数为顺时针，负数为逆时针
+}
+```
+
+## imageoprate.gesture.js 参数及说明
+* 依赖 gesture.js
+* 支持移动端和 PC 端
+* 暂不支持旋转
+
+```js
+config = {          // 默认配置
+  zoomspeed: 10,    // 缩放速度
+  maxWidth: 1024,   // 最大宽度
+  minWidth: 100     // 最小宽度
+}
+```
+
+## 例子
+* [imageoprate.js 例子](https://bigfact.github.io/frontforge/things/imageoprate/index.html)
+
+```html
+<button onclick="clockwise()">顺时针</button>
+<button onclick="anticlockwise()">逆时针</button>
+<img src="https://avatars1.githubusercontent.com/u/11718965?v=3&s=460" style="position: absolute;">
+<script src="src/imageoprate.js"></script>
+<script>
+    var index = 0;
+    var imgs = document.getElementsByTagName('img');
+    var imgops = [];
+    for(var i = 0; i < imgs.length; i++) {
+        imgops[i] = new ImageOprate(imgs[i]);
+        imgops[i].init();
+    }
+    function clockwise () {
+        imgops[index].rotate(true);
+    }
+    function anticlockwise () {
+        imgops[index].rotate(false);
+    }
+</script>
+```
+
+* [imageoprate.gesture.js 例子](https://bigfact.github.io/frontforge/things/imageoprate/index.gesture.html)
+
+```html
+<img src="https://avatars1.githubusercontent.com/u/11718965?v=3&s=460" style="position: absolute;">
+<script src="../gesture/dist/gesture.min.js"></script>
+<script src="src/imageoprate.gesture.js"></script>
+<script>
+    var img = document.getElementsByTagName('img')[0];
+    var imgp = new ImageOprate(img);
+    img.onload = function() {
+      imgp.init();
+    }
+</script>
+```
---
+++ things/imageoperate/dist/imageoprate.gesture.js
@@ -0,0 +1,83 @@
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// author: "bigfact <bigfact0@gmail.com>"
+// date: 2016.04.29
+// 依赖 ../gesture/dist/gesture.min.js
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+(function (window) {
+    // 构造函数
+    function ImageOprate(obj) {
+        var _this = this;
+        _this.obj = obj;         // 当前预览对象
+        _this.gesture = new Gesture(_this.obj);
+        _this.scale = 1;
+        _this.first = {};
+        _this.size = {};
+        _this.config = {         // 默认配置
+            zoomspeed: 10,       // 缩放速度
+            maxWidth: 1024,     // 最大宽度
+            minWidth: 100      // 最小宽度
+        }
+    }
+    // 方法
+    ImageOprate.prototype = {
+        // 初始化
+        init: function (config) {
+            var _this = this;
+
+            _this.size = {
+                w: _this.obj.clientWidth,
+                h: _this.obj.clientHeight
+            }
+
+            // 配置
+            config = config || {};
+            _this.config.zoomspeed = config.zoomspeed || _this.config.zoomspeed;
+            _this.config.maxWidth = config.maxWidth || _this.config.maxWidth;
+            _this.config.minWidth = config.minWidth || _this.config.minWidth;
+
+            _this.gesture.init({
+                start: function () {
+                    _this.start(_this);
+                },
+                doing: function () {
+                    _this.doing(_this);
+                },
+                finish: function () {
+                    _this.finish(_this);
+                }
+            });
+
+        },
+        start: function (_this) {
+            _this.first = {
+                x: _this.obj.offsetLeft - _this.gesture.points.first[0].x,
+                y: _this.obj.offsetTop - _this.gesture.points.first[0].y
+            }
+        },
+        doing: function (_this) {
+            var type = _this.gesture.config.type;
+            var type2 = _this.gesture.type;
+            if (type2 == type.zoom) {
+                _this.zoom(_this);
+            }
+            else if (type2 == type.up || type2 == type.down || type2 == type.left || type2 == type.right) {
+                _this.obj.style.left = _this.gesture.points.current[0].x + _this.first.x + 'px';
+                _this.obj.style.top = _this.gesture.points.current[0].y + _this.first.y + 'px';
+            }
+        },
+        finish: function (_this) {
+            _this.first = {};
+        },
+        zoom: function (_this) {
+            _this.scale = 1 + _this.gesture.scale * _this.config.zoomspeed / 1000;
+            var w = _this.size.w * _this.scale;
+            if (w >= _this.config.minWidth && w <= _this.config.maxWidth) {
+                _this.obj.style.width = w + 'px';
+                _this.obj.style.height = (_this.size.h * _this.scale) + 'px';
+            }
+        },
+    }
+    window.ImageOprate = ImageOprate;
+    return ImageOprate;
+})(window);
